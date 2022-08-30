80c9b520d4fac7ebd0aedd49a46fee3a9613e2f5
Louis Stephens <lstephens@chumneyads.com>
2022-08-30 10:30:51 -0400

    Css and js output bundled
    Moved css and js to separate bundled files.

---
+++ src/js/dev.js
@@ -0,0 +1,7 @@
+// import './src/scss/demo.scss?inline'
+import '../scss/dev.scss'
+console.log('Hello World 2')
+// document.querySelector('#app').innerHTML = `
+//   <h1>Hello Vite!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `
--- vite.config.js
+++ vite.config.js
@@ -8,18 +8,58 @@ export default defineConfig({
     
         //outDir: './dist',
         rollupOptions: {
+            input: {
+                // index: './index.html',
+                app: './index.html',
+                dev: './src/js/dev.js',
+            },
             output: {
-                entryFileNames: "assets/js/[name].js",
-                chunkFileNames: "assets/[ext]/[name].js",
-                // assetFileNames: "assets/[ext]/[name].[ext]",
+                entryFileNames: "js/[name].js",
+                chunkFileNames: "[name].js",
+                // assetFileNames: "[ext]/[name].[ext]",
                 assetFileNames: (assetInfo) => {
                     let extType = assetInfo.name.split('.').at(1);
                     if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
                         extType = 'images';
                     }
-                    return `assets/${extType}/[name][extname]`;
+                    if (assetInfo.name === 'index.css') {
+                        console.log('fjfjfjfjf');
+                        return assetInfo.name = `${extType}/app.css`;
+                    }
+
+                    return `${extType}/[name][extname]`;
                 },
             }
         }
     }
 })
+
+
+
+
+// Customisation: vite.config.js
+// const path = require('path')
+// import { defineConfig } from 'vite'
+
+// export default defineConfig({
+//     base: "",
+//     build: {
+//         // target: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1' ],
+//         lib: {
+//             entry: path.resolve(__dirname, 'src/js/app.js'),
+//             name: 'app',
+//             fileName: (format) => `app.min.js`,
+//         },
+//         rollupOptions: {
+//             output: {
+//                 assetFileNames: (assetInfo) => {
+//                     if (assetInfo.name === 'style.css') return 'app.min.css';
+//                     return assetInfo.name;
+//                 },                
+//             }
+//         }
+
+    
+
+//     }
+// })
